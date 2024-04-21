8bca1bc02a9168462855ac2e4b55d0c22340e0fd
deco-L <scotchwitchbroom@gmail.com>
2024-04-21 14:03:55 +0900

    V V

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
+/*   Updated: 2024/04/20 20:58:40 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef CLAPTRAP_HPP
+
+#include <cstdlib>
+#include <iostream>
+
+class ClapTrap {
+private:
+  std::string name;
+  long long   hp;
+  long long   ep;
+  long long   ad;
+
+public:
+  ClapTrap    ( void );
+  ClapTrap    ( const ClapTrap& obj );
+  ClapTrap    ( const std::string& name );
+  ~ClapTrap   ( void );
+
+  void        attack( const std::string& target );
+  void        takeDamage( unsigned int amount );
+  void        beRepaired( unsigned int amount );
+
+  ClapTrap&   operator=(const ClapTrap& obj);
+};
+
+#endif
